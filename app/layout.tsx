75e23450801f62e9d037320340295d34c9c11dc8
Unnati Samaiyar <104674490+UnnatiSamaiyar@users.noreply.github.com>
2026-01-15 17:46:54 +0530

    Add Google site verification meta tag
    Added a meta tag for Google site verification in the RootLayout component.

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,9 +19,13 @@ import Providers from "./providers";
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <head>
+      <meta name="google-site-verification" content="kcfXCAoRsUchh23GlVtrz0qlTjpINXrYz_DiE8oQhR4" />
+      </head>
       <body>
         <Providers>{children}</Providers>
       </body>
     </html>
   );
 }
+
